0f97d58ecdd5630b585d50cd36d8c6b05012d524
graysonlee123 <graysonleegantek@gmail.com>
2020-02-07 17:21:22 -0500

    adds basic api setup

---
+++ routes/api.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/timeline', (req, res, next) => {});
+
+router.get('/timeline/edit/:id', (req, res, next) => {});
+
+module.exports = router;

c8d79c816eb3c0b4d1e7da9d8df08bc4569b40df
graysonlee123 <graysonleegantek@gmail.com>
2020-02-09 13:38:08 -0500

    renders quick navigation dates

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -21,6 +21,21 @@ export default class Sidebar extends Component {
     const events = this.state.events;
     const sidebar = {};
 
+    const monthNames = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+
     events &&
       events.forEach(e => {
         const year = new Date(e.date).getFullYear();
@@ -29,35 +44,35 @@ export default class Sidebar extends Component {
         if (sidebar[year] == undefined) sidebar[year] = {};
         if (sidebar[year][month] == undefined) sidebar[year][month] = [];
         sidebar[year][month].push(e);
-
-        console.log(sidebar);
       });
 
     return (
       <div id='sidebar'>
         <div className='quick-navigaton'>
-          {/* {years.map(y => (
-            <div className='year-container'>
-              <div className='year-wrapper'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='6.385'
-                  height='6.769'
-                  viewBox='0 0 6.385 6.769'
-                >
-                  <path
-                    id='Subtraction_3'
-                    data-name='Subtraction 3'
-                    d='M204,35.385h0L200.615,29h6.769L204,35.385Z'
-                    transform='translate(-29 207.385) rotate(-90)'
-                    fill='#313131'
-                  />
-                </svg>
-                {y}
+          <div className='year-container'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='6.385'
+              height='6.769'
+              viewBox='0 0 6.385 6.769'
+            >
+              <path
+                id='Subtraction_3'
+                data-name='Subtraction 3'
+                d='M204,35.385h0L200.615,29h6.769L204,35.385Z'
+                transform='translate(-29 207.385) rotate(-90)'
+                fill='#313131'
+              />
+            </svg>
+            {Object.keys(sidebar).map((year, i) => (
+              <div key={i} className='months-wrapper'>
+                <div>{year}</div>
+                {Object.keys(sidebar[year]).map((month, i) => (
+                  <div key={i}>{monthNames[month - 1]}</div>
+                ))}
               </div>
-              <div className='months-wrapper'></div>
-            </div>
-          ))} */}
+            ))}
+          </div>
         </div>
         <div className='new-event'>+ New Event</div>
       </div>

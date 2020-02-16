00cf2a4fb8fcf75f00bfda722a6e7e1add01cc7c
Grayson Gantek <graysonleegantek@gmail.com>
2020-02-16 01:03:38 -0500

    check for existing user

--- config/passport.js
+++ config/passport.js
@@ -1,19 +1,32 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../models/User');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User");
 
-require('dotenv').config();
+require("dotenv").config();
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENTID,
-    clientSecret: process.env.SECRET,
-    callbackURL: '/auth/google/redirect'
-}, (accesstoken, refreshToken, profileInformation, done) => {
-    new User ({
-        username: profileInformation.displayName,
-        googleId: profileInformation.id
-    }).save().then((newUser) => {
-        console.log('New user created!', newUser)
-    })
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENTID,
+      clientSecret: process.env.SECRET,
+      callbackURL: "/auth/google/redirect"
+    },
+    (accesstoken, refreshToken, profileInformation, done) => {
+      // Check if user already exists in database
+      User.findOne({ googleId: profileInformation.id }).then(currentUser => {
+        if (currentUser) {
+          console.log('User already exists: ', currentUser);
+        } else {
+          new User({
+            username: profileInformation.displayName,
+            googleId: profileInformation.id
+          })
+            .save()
+            .then(newUser => {
+              console.log("New user created!", newUser);
+            });
+        }
+      });
     }
-));
+  )
+);

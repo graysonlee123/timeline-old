1de4f1fef4e879eb7669c78154d40bb1edb1467b
Grayson Gantek <graysonleegantek@gmail.com>
2020-02-17 18:21:25 -0500

    basic authentication component; still need to pass in if user is auth

--- client/src/components/Routing/PrivateRoute.js
+++ client/src/components/Routing/PrivateRoute.js
@@ -1,27 +1,48 @@
 import React, { Component } from "react";
 import { Route, Redirect } from "react-router-dom";
 import axios from "axios";
+import Spinner from "../Spinner";
 
-export default class PrivateRoute extends Component {
-  state = {
-    isLoading: true,
-    isAuthenticated: false
-  }
+const PrivateRoute = ({component: Component, ...rest}) => (
+  <Route
+    {...rest}
+    render={props => 
+      true ? (
+        <Component {...props} />
+      ) : ( 
+        <Redirect to='/login'/>
+      )
+    }
+  />
+)
 
-  componentDidMount = () => {
-    axios.get('/auth/validate')
-      .then(data => {
-        console.log('Validation: ', data);
-        this.setState({isLoading: false, isAuthenticated: true});
-      }).catch(err => {
-        console.log(err);
-        this.setState({isLoading: false});
-      })
-  }
+export default PrivateRoute;
 
-  render() {
-    return (
-      !this.state.isLoading && this.state.isAuthenticated ? (<div>Authenticated!</div>) : (<div>Not authenticated.</div>)
-    )
-  }
-}
+// export default class PrivateRoute extends Component {
+//   state = {
+//     isLoading: true,
+//     isAuthenticated: false
+//   }
+
+//   componentDidMount = () => {
+//     axios.get('/auth/validate')
+//       .then(data => {
+//         console.log('Validation: ', data);
+//         this.setState({isLoading: false, isAuthenticated: true});
+//       }).catch(err => {
+//         console.log(err);
+//         this.setState({isLoading: false});
+//       })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//        {this.state.isLoading && <Spinner />}
+//        {!this.state.isLoading && !this.state.isAuthenticated && <Redirect to='/login'/>}
+//        {this.state.isAuthenticated && this.props.component}
+//         {/* {!this.state.isLoading && this.state.isAuthenticated ? (<div>Authenticated!</div>) : (<div>Not authenticated.</div>)} */}
+//       </div>
+//     )
+//   }
+// }

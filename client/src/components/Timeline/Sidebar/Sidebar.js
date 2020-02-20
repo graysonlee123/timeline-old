b9f64359de67100a2eee8149e3d053d32cdf1855
Grayson Gantek <graysonleegantek@gmail.com>
2020-02-20 12:31:39 -0500

    now renders accordian sidebar properly

--- client/src/components/Timeline/Sidebar/Sidebar.js
+++ client/src/components/Timeline/Sidebar/Sidebar.js
@@ -1,134 +1,145 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import classNames from 'classnames';
+import React, { Component } from "react";
+import axios from "axios";
+import classNames from "classnames";
 
-import Spinner from '../../Spinner';
-import NewEvent from '../NewEvent/NewEvent';
+import Spinner from "../../Spinner";
+import NewEvent from "../NewEvent/NewEvent";
 
 export default class Sidebar extends Component {
-  state = {
-    years: [],
-    loading: true,
-    activeDate: {
-      year: '',
-      month: ''
-    }
-  };
-
-  componentDidMount = () => {
-    axios
-      .get('/api/events')
-      .then(data => {
-        this.setState({
-          events: data.data,
-          loading: false
-        });
-      })
-      .catch(err => console.log(err));
-  };
+  // componentDidMount = () => {
+  //   axios
+  //     .get('/api/events')
+  //     .then(data => {
+  //       this.setState({
+  //         events: data.data,
+  //         loading: false
+  //       });
+  //     })
+  //     .catch(err => console.log(err));
+  // };
 
   handleAccordian(e) {
     const elem = e.currentTarget;
     const months = elem.nextElementSibling;
 
-    if (elem.classList.contains('accordian-active')) {
-      elem.classList.remove('accordian-active');
-      months.style.display = 'none';
+    if (elem.classList.contains("accordian-active")) {
+      elem.classList.remove("accordian-active");
+      months.style.display = "none";
     } else {
-      elem.classList.add('accordian-active');
-      months.style.display = 'block';
+      elem.classList.add("accordian-active");
+      months.style.display = "block";
     }
   }
 
-  handleChangeDate = e => {
-    const elem = e.currentTarget;
+  // handleChangeDate = e => {
+  //   const elem = e.currentTarget;
 
-    this.setState({
-      activeDate: {
-        year: elem.dataset.eventyear,
-        month: elem.dataset.eventmonth
-      }
-    });
-  };
+  //   this.setState({
+  //     activeDate: {
+  //       year: elem.dataset.eventyear,
+  //       month: elem.dataset.eventmonth
+  //     }
+  //   });
+  // };
 
   render() {
-    const events = this.state.events;
-    const sidebar = {};
     const monthNames = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December'
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
     ];
+    // const events = this.state.events;
+    // const sidebar = {};
+    // // const monthNames = [
+    // //   'January',
+    // //   'February',
+    // //   'March',
+    // //   'April',
+    // //   'May',
+    // //   'June',
+    // //   'July',
+    // //   'August',
+    // //   'September',
+    // //   'October',
+    // //   'November',
+    // //   'December'
+    // // ];
 
-    events &&
-      events.forEach(e => {
-        const year = new Date(e.date).getFullYear();
-        const month = new Date(e.date).getMonth();
+    // events &&
+    //   events.forEach(e => {
+    //     const year = new Date(e.date).getFullYear();
+    //     const month = new Date(e.date).getMonth();
 
-        if (sidebar[year] === undefined) sidebar[year] = {};
-        if (sidebar[year][month] === undefined) sidebar[year][month] = [];
-        sidebar[year][month].push(e);
-      });
+    //     if (sidebar[year] === undefined) sidebar[year] = {};
+    //     if (sidebar[year][month] === undefined) sidebar[year][month] = [];
+    //     sidebar[year][month].push(e);
+    //   });
 
     return (
-      <aside>
-        <div className='accordians-wrapper'>
-          {this.state.loading && <Spinner />}
-          {Object.keys(sidebar).map((year, i) => (
-            <div className='accordian' key={i}>
-              <div className='accordian-year' onClick={this.handleAccordian}>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='6.385'
-                  height='6.769'
-                  viewBox='0 0 6.385 6.769'
-                >
-                  <path
-                    id='Subtraction_3'
-                    data-name='Subtraction 3'
-                    d='M204,35.385h0L200.615,29h6.769L204,35.385Z'
-                    transform='translate(-29 207.385) rotate(-90)'
-                    fill='#313131'
-                  />
-                </svg>
-                <span>{' ' + year}</span>
-              </div>
-              <div className='accordian-months'>
-                {Object.keys(sidebar[year]).map((month, i) => (
-                  <div
-                    key={i}
-                    className={classNames({
-                      'accordian-month': true,
-                      'active-month':
-                        this.state.activeDate.year === year &&
-                        this.state.activeDate.month === month
-                    })}
-                    data-eventyear={year}
-                    data-eventmonth={month}
-                    onClick={this.handleChangeDate}
+      <div className='accordian_container'>
+        <div className="accordians-wrapper">
+          {this.props.isLoading ? (
+            <Spinner />
+          ) : (
+            Object.keys(this.props.accordianLayout).map((year, i) => (
+              <div className="accordian" key={i}>
+                <div className="accordian-year" onClick={this.handleAccordian}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="6.385"
+                    height="6.769"
+                    viewBox="0 0 6.385 6.769"
                   >
-                    {monthNames[month]}
-                    <span>
-                      {sidebar[year][month].length} Event
-                      {sidebar[year][month].length > 1 && 's'}
-                    </span>
-                  </div>
-                ))}
+                    <path
+                      id="Subtraction_3"
+                      data-name="Subtraction 3"
+                      d="M204,35.385h0L200.615,29h6.769L204,35.385Z"
+                      transform="translate(-29 207.385) rotate(-90)"
+                      fill="#313131"
+                    />
+                  </svg>
+                  <span>{" " + year}</span>
+                </div>
+                <div className="accordian-months">
+                  {Object.keys(this.props.accordianLayout[year]).map(
+                    (month, i) => (
+                      <div
+                        key={i}
+                        className={classNames({
+                          "accordian-month": true,
+                          "active-month":
+                            this.props.activeDate.year === parseInt(year) &&
+                            this.props.activeDate.month === parseInt(month)
+                        })}
+                        data-eventyear={year}
+                        data-eventmonth={month}
+                        onClick={this.props.handleChangeDate}
+                      >
+                        {monthNames[month]}
+                        <span>
+                          {this.props.accordianLayout[year][month].length} Event
+                          {this.props.accordianLayout[year][month].length > 1 &&
+                            "s"}
+                        </span>
+                      </div>
+                    )
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
         <NewEvent />
-      </aside>
+      </div>
     );
   }
 }
--- client/src/components/Timeline/Timeline.js
+++ client/src/components/Timeline/Timeline.js
@@ -1,44 +1,98 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Sidebar from './Sidebar/Sidebar';
-import AppHeader from './AppHeader';
-import Card from './Card';
-import Spinner from '../Spinner';
+import Sidebar from "./Sidebar/Sidebar";
+import AppHeader from "./AppHeader";
+import Card from "./Card";
+import Spinner from "../Spinner";
 
 export default class Login extends Component {
   state = {
-    isLoading: true
-  }
+    isLoading: true,
+    fetchEventsData: [],
+    activeDate: {
+      year: "",
+      month: ""
+    }
+  };
 
   componentDidMount = async () => {
     try {
-      const events = await fetch('/event/my-events');
-      const res = await events.json()
+      const events = await fetch("/event/my-events");
+      const res = await events.json();
 
       if (res) {
         console.log(res);
-        // Render components
-        this.setState({events: res, isLoading: false})
+
+        // Figure out the years
+        const accordianLayout = {};
+
+        res &&
+          res.forEach(event => {
+            const year = new Date(event.date).getFullYear();
+            const month = new Date(event.date).getMonth();
+
+            if (accordianLayout[year] === undefined) accordianLayout[year] = {};
+            if (accordianLayout[year][month] === undefined)
+              accordianLayout[year][month] = [];
+            accordianLayout[year][month].push(event);
+          });
+
+        console.log(accordianLayout);
+
+        // Render components by setting all the data
+        this.setState({
+          fetchEventsData: res,
+          isLoading: false,
+          activeDate: {
+            year: new Date(res[0].date).getFullYear(),
+            month: new Date(res[0].date).getMonth()
+          },
+          accordianLayout: accordianLayout
+        });
       }
-    } catch(err) {
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
+
+  handleChangeDate = e => {
+    const elem = e.currentTarget;
+
+    this.setState({
+      activeDate: {
+        year: elem.dataset.eventyear,
+        month: elem.dataset.eventmonth
+      }
+    });
+  };
 
   render() {
     return (
-      <div className='timeline_wrapper'>
-        <Sidebar />
+      <div className="timeline_wrapper">
+        <Sidebar
+          handleChangeDate={this.handleChangeDate}
+          accordianLayout={this.state.accordianLayout}
+          isLoading={this.state.isLoading}
+          activeDate={this.state.activeDate}
+        />
         <main>
           <AppHeader />
-          <div className='cards_container'>
-            {this.state.isLoading ? <Spinner /> : this.state.events.map((event, i) => {
-              const {date, name, description} = event;
-              
-              return (
-                <Card name={name} date={date} description={description} key={i}/>
-              )
-            }
+          <div className="cards_container">
+            {this.state.isLoading ? (
+              <Spinner />
+            ) : (
+              this.state.fetchEventsData.map((event, i) => {
+                const { date, name, description } = event;
+
+                return (
+                  <Card
+                    name={name}
+                    date={date}
+                    description={description}
+                    key={i}
+                  />
+                );
+              })
             )}
           </div>
         </main>

551ced6bf6bf7db3d99012e655243007662ccb6a
graysonlee123 <graysonleegantek@gmail.com>
2020-03-15 19:42:13 -0400

    sets up redux store

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,29 @@ import React, {Fragment} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+// Components
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <Switch>
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;

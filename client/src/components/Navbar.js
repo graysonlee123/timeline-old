5f0f560b72fefac52433bf1bce23c722b8155c0f
Grayson Gantek <graysonleegantek@gmail.com>
2020-03-16 22:06:31 -0400

    adds logout action and reducer; conditionally rendering navbar based on login status

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../actions/auth";
 
-const Navbar = () => {
+const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <a href="#!" onClick={logout}>
+          {" "}
+          Logout
+        </a>
+      </li>
+      <li>
+        <Link to="/profile">My Profile</Link>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+    </ul>
+  );
+
   return (
     <nav>
-      <span className='nav-left'>
-        <Link to='/'>Home</Link>
+      <span className="nav-left">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
       </span>
-      <span className='nav-right'>
-        <Link to='/login'>Login</Link>
+      <span className="nav-right">
+        {isAuthenticated ? authLinks : guestLinks}
       </span>
     </nav>
   );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);

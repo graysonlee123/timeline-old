284c21da54e67422ea7ce2fd2bff560b01bfec47
graysonlee123 <graysonleegantek@gmail.com>
2020-03-19 16:53:04 -0400

    adds remove alert button on the alert popup

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,8 +1,12 @@
-import { v4 as uuidv4 } from "uuid";
-import { SET_ALERT, REMOVE_ALERT } from "./types";
+import { v4 as uuidv4 } from 'uuid';
+import { SET_ALERT, REMOVE_ALERT } from './types';
 
 // The dispatch is thanks to the thunk middleware
-export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
+export const setAlert = (
+  msg,
+  alertType = 'neutral',
+  timeout = 5000
+) => dispatch => {
   const id = uuidv4();
 
   dispatch({
@@ -17,4 +21,9 @@ export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
   setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
 
-export const test = 1;
+export const closeAlert = id => dispatch => {
+  dispatch({
+    type: REMOVE_ALERT,
+    payload: id 
+  });
+};

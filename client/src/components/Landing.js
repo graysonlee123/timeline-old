8b31bf4ababf636aa1717bb738f7a87fe48305ec
graysonlee123 <graysonleegantek@gmail.com>
2020-03-19 13:27:02 -0400

    working on sass; adds some new mixins

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
-    <div>
-      
+    <div className='landing'>
+      <section className="hero">
+        <div className="container">
+          <h1>Some sort of Tagline Here</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            convallis, metus sit amet finibus placerat, lacus sapien laoreet
+            risus, quis porta ligula ipsum sit amet erat. Quisque et feugiat
+            turpis. Mauris tincidunt nunc quam, in dictum.
+          </p>
+          <Link to='/register' className="button">Sign Up for Free</Link>
+        </div>
+      </section>
+      <section className='is-bright'>
+        <div className="container">
+          <div className='benefits-wrapper'>
+            <figure>
+              <h3>Header here</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                convallis, metus sit amet finibus placerat, lacus sapien laoreet
+                risus, quis porta ligula ipsum.
+              </p>
+            </figure>
+            <figure>
+              <h3>Header here</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                convallis, metus sit amet finibus placerat, lacus sapien laoreet
+                risus, quis porta ligula ipsum.
+              </p>
+            </figure>
+            <figure>
+              <h3>Header here</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                convallis, metus sit amet finibus placerat, lacus sapien laoreet
+                risus, quis porta ligula ipsum.
+              </p>
+            </figure>
+          </div>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
